f3f8562c427e8228a3f9cdfe3466dd8fa0dcdaac
mamoru0101 <d08020386730@gmail.com>
2021-07-20 20:10:03 +0900

    検索結果の表示

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { fetchSearchData } from '../apis'
 import Layout from "../components/Layout/Layout"
+import { Store } from '../store'
+import VideoGrid from '../components/VideoGrid/VideoGrid'
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem'
 
 const Search = () => {
+    const location = useLocation()
+    const {globalState, setGlobalState} = useContext(Store)
+    const setSearchResult = async () => {
+        const searchParams = new URLSearchParams(location.search)
+        const query = searchParams.get('query')
+        
+        if (query) {
+            await fetchSearchData(query).then(res => {
+                setGlobalState({type: 'SET_SEARCHED', payload: {searched: res.data.items}})
+            })
+        }
+
+    }
+    useEffect(() => {
+        setSearchResult()
+    }, [])
+    
     return (
         <Layout>
-            Search page  
+            <VideoGrid>
+            {globalState.searched && globalState.searched.map(search => {
+                return (
+                    <VideoGridItem 
+                        id={search.id.videoId}
+                        key={search.id.videoId}
+                        src={search.snippet?.thumbnails.medium.url}
+                        title={search.snippet?.title}
+                    />
+                )
+            })}
+            </VideoGrid>
         </Layout>
     )
 }
